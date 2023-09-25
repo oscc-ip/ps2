c978712624017902e470158ea7c4bec41019700d
maksyuki <maksyuki@126.com>
2023-09-25 20:47:09 +0800

    feat: add apb4 interface to the ps2 kdb fifo

--- rtl/apb4_ps2_keyboard.sv
+++ rtl/apb4_ps2_keyboard.sv
@@ -18,6 +18,50 @@ module apb4_ps2_keyboard (
     output logic  irq_o
 );
 
+  logic [7:0] r_fifo[0:7];
+  logic [2:0] r_wr_ptr, r_rd_ptr;
+  logic [9:0] r_buf;
+  logic [3:0] r_cnt;
+  logic [2:0] r_clk_sync;
+  logic       s_fall_edge;
+  logic [7:0] r_tmp_outdat;
+  always_ff @(posedge apb4.hclk) begin
+    r_clk_sync <= {r_clk_sync[1:0], ps2_clk_i};
+  end
+
+  assign s_fall_edge = r_clk_sync[2] & (~r_clk_sync[1]);
+
+  always_ff @(posedge apb4.hclk) begin
+    if (~abp4.hresetn) begin
+      r_cnt    <= '0;
+      r_wr_ptr <= '0;
+    end else begin
+      if (s_fall_edge) begin
+        if (r_cnt == 4'd10) begin
+          if ((r_buf[0] == 0) && ps2_dat_i && (^r_buf[9:1])) begin
+            r_fifo[r_wr_ptr] <= r_buf[8:1];
+            r_wr_ptr         <= r_wr_ptr + 1'b1;
+          end
+          r_cnt <= '0;
+        end else begin
+          r_buf[r_cnt] <= ps2_dat_i;
+          r_cnt        <= r_cnt + 1'b1;
+        end
+      end
+    end
+  end
+
+  assign apb4.prdata = {24'b0, r_tmp_outdat};
+  always_ff @(posedge apb4.hclk, negedge abp4.hresetn) begin
+    if (~apb4.hresetn) begin
+      r_rd_ptr     <= '0;
+      r_tmp_outdat <= '0;
+    end else if ((apb4.psel && apb4.penable) && (~apb4.pwrite)) begin
+      r_tmp_outdat <= (r_rd_ptr == r_wr_ptr) ? '0 : r_fifo[r_rd_ptr];
+      r_rd_ptr     <= (r_rd_ptr == r_wr_ptr) ? r_rd_ptr : r_rd_ptr + 1'b1;
+    end
+  end
+
   assign apb4.pready = 1'b1;
   assign apb4.pslerr = 1'b0;
 
